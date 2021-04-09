8edd33d9829a68f7172f73c29c722d0a75c88be4
guruhedipurwanto <guruhedipurwanto@gmail.com>
2021-04-09 23:34:30 +0800

    upgrade packages and add title tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import { Greeting } from '../components/Greeting';
 import { Header } from '../components/Header';
 import { ProjectsGallery } from '../components/ProjectsGallery';
+import Head from 'next/head';
 
 const Home: React.FC = () => {
   return (
     <>
+      <Head>
+        <title>Guruh Edi Purwanto</title>
+      </Head>
       <Header />
       <Greeting />
       <ProjectsGallery />
