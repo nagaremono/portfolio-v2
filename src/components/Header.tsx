6daeb1528abfaed78aafaee103cc5db890a2df02
guruhedipurwanto <guruhedipurwanto@gmail.com>
2021-04-16 00:38:05 +0800

    add responsive styling to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,21 +5,38 @@ import { AiFillGithub, AiOutlineTwitter } from 'react-icons/ai';
 export const Header: React.FC = () => {
   return (
     <Flex
-      px={2}
+      px={[2, 4, 8]}
       bg="gray.800"
       height={20}
       alignItems="center"
       justifyContent="space-between"
     >
-      <Heading fontFamily="fira" color="gray.300" fontSize={['md']}>
+      <Heading
+        fontFamily="fira"
+        color="gray.300"
+        fontSize={{ base: 'md', sm: 'xl' }}
+      >
         guruh.edi.purwanto;
       </Heading>
-      <Flex justifyContent="space-around" w={1 / 4}>
+      <Flex
+        justifyContent="space-around"
+        w={{ base: 1 / 4, sm: 1 / 5, md: 1 / 6, lg: 1 / 8, xl: 1 / 12 }}
+      >
         <Link href="https://www.github.com/nagaremono" isExternal>
-          <Icon w={10} h={8} color={'white'} as={AiFillGithub} />
+          <Icon
+            w={[10, 12, 14]}
+            h={[8, 10]}
+            color={'white'}
+            as={AiFillGithub}
+          />
         </Link>
         <Link href="https://www.twitter.com/guruhedip" isExternal>
-          <Icon w={10} h={8} color={'white'} as={AiOutlineTwitter} />
+          <Icon
+            w={[10, 12, 14]}
+            h={[8, 10]}
+            color={'white'}
+            as={AiOutlineTwitter}
+          />
         </Link>
       </Flex>
     </Flex>
