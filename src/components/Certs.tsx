f261b46122f2809cddf4497748a7e6c0135ce9f7
guruhedipurwanto <guruhedipurwanto@gmail.com>
2021-04-16 23:39:59 +0800

    set responsiveness for certificate section

--- src/components/Certs.tsx
+++ src/components/Certs.tsx
@@ -4,27 +4,39 @@ import React from 'react';
 
 export const Certs: React.FC = () => {
   return (
-    <Box backgroundColor="gray.600" p={4}>
-      <Heading as="h3" color="white" fontFamily="fira" fontSize="xl">
+    <Box backgroundColor="gray.600" px={4} py={8} my={8}>
+      <Heading
+        as="h3"
+        color="white"
+        fontFamily="fira"
+        fontSize={['xl', '2xl']}
+        px={[0, 2, 6, 12]}
+      >
         Certificates
       </Heading>
-      <Flex mt={2} flexDirection="column" px={4}>
-        <Box my={2}>
+      <Flex
+        mt={2}
+        flexDirection={['column', 'row']}
+        justifyContent={{ sm: 'space-evenly' }}
+        width={{ lg: '80%', xl: '75%', '2xl': '60%' }}
+        px={[4, 2]}
+      >
+        <Flex justifyContent="center" my={2} width={{ sm: '45%', md: '40%' }}>
           <Image
             width={350}
             height={245}
             alt="Sertifikat Menjadi Front-End Developer Expert"
             src="/expert-cert.jpg"
           />
-        </Box>
-        <Box my={2}>
+        </Flex>
+        <Flex justifyContent="center" my={2} width={{ sm: '45%', md: '40%' }}>
           <Image
             width={350}
             height={245}
             alt="Sertifikat Full Stack Open"
             src="/fullstack.jpg"
           />
-        </Box>
+        </Flex>
       </Flex>
     </Box>
   );
